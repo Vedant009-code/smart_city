c228d09ad4160d27dd0f1c94ed8ff21886ee82ac
Vedant009-code <vedantkokane445@gmail.com>
2026-02-28 16:06:38 +0530

    login

--- smart_city/app/login/page.jsx
+++ smart_city/app/login/page.jsx
@@ -36,7 +36,6 @@ export default function LoginPage() {
   }
 
   return (
-
     <div className="flex items-center justify-center min-h-screen bg-gray-50">
 
       <div className="w-full max-w-md p-6 bg-white rounded-lg shadow">
